4aa24ebdf8360c1065f38466bec7006885e6ce40
Abigail <asexton5@nd.edu>
2021-06-03 14:45:21 -0400

    display on js

--- server.js
+++ server.js
@@ -18,3 +18,75 @@ const connection = mysql.createConnection({
     password: '***********',
     database: 'employee_db'
 })
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId + '\n');
+    startApp();
+});
+
+// Start app with npm run
+const startApp = () => {
+
+    // Main menu questions
+    const mainQuestions = () => {
+        return inquirer.prompt([
+            {
+                type: 'list',
+                name: 'menu',
+                message: 'What would you like to do?',
+                choices: [
+                    'Finished',
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
+                    'Update an Employee Role'
+                ],
+            }
+
+        ])
+            .then(answers => {
+                if (answers.menu === 'View All Departments') {
+                    displayDepartments();
+                }
+                if (answers.menu === 'View All Roles') {
+                    displayRoles();
+                }
+                if (answers.menu === 'View All Employees') {
+                    displayEmployees();
+                }
+                if (answers.menu === 'Add a Department') {
+                    addDepartment();
+                }
+                if (answers.menu === 'Add a Role') {
+                    addRole();
+                }
+                if (answers.menu === 'Add an Employee') {
+                    addEmployee();
+                }
+                if (answers.menu === 'Update an Employee Role') {
+                    updateRole();
+                }
+                if (answers.menu === 'Finished') {
+                    console.log('Goodbye!')
+                    return false;
+                }
+
+            })
+    };
+
+    // Display all Departments
+    displayDepartments = () => {
+        const query = connection.query(
+            'SELECT department.id, department_name AS Department FROM department',
+            function (err, res) {
+                if (err) throw err;
+                console.log('\n Displaying all Departments...\n');
+                console.table(res);
+                mainQuestions();
+                return true;
+            }
+        );
+    };
